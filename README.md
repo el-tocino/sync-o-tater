bac8e218a23ce3aa9a8ac323fa72969bc9d24e03
el-tocino <email+github@aforkinthetoad.com>
2015-11-01 15:30:25 -0600

    Schpongletronics

---
+++ README.md
@@ -0,0 +1,13 @@
+# sync-o-pater
+merge two videos into one side-by-side audio-synced 3 d monstrosity.
+
+Requires ffmpeg (with h264 support at the moment)
+https://www.ffmpeg.org/
+
+and clapperless
+http://users.mur.at/ms/projects/clapperless/
+(scipy and nimby needed. python2 based)
+
+and exiftool
+http://www.sno.phy.queensu.ca/~phil/exiftool/
+
---
+++ syncotater.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+# $1 = left video
+# $2 = right video
+
+#adjust to point at your copy
+CLPR=~/clapperless.py
+
+# boring test stuff
+
+if [ $# -ne 2 ]
+	then
+		echo "Usage: $0 leftvideofile rightvideofile"
+		exit 1
+if [ ! -r $1 ]
+	then	
+		echo "Can't read left video?"
+		exit 2
+fi
+if [ ! -r $2 ]
+	then
+		echo "Can't read right video?"
+		exit 2
+fi
+
+if [ -x $CLPR ]
+	then
+		echo "Can't exec clapperless!"
+		exit 3
+fi
+
+# find framerate of the videos
+
+LFR=$(exiftool -VideoFrameRate $1 | awk ' { print $NF} ' )
+RFR=$(exiftool -VideoFrameRate $2 | awk ' { print $NF} ' )
+
+if [ $LFR != $RFR ]
+	then
+		echo "Differing frame rates. This tool needs matched frame rates for now."
+		exit 4 
+fi
+
+# clapperless hates fractional framerates.
+case ${RFR} in
+	"29.97")
+		LFR=30
+		;;
+	"59.94")
+		LFR=60
+		;;
+	"47.8")
+		LFR=48
+		;;
+	"119.88")
+		LFR=120
+		;;
+	"239.76")
+		LFR=240
+	;;
+
+esac
+
+
+LOF=$(python2 ${CLPR} -c -r ${LFR} $1 $2 | tail -1 | awk ' { print $1 } ')
+ROF=$(python2 ${CLPR} -c -r ${LFR} $2 $1 | tail -1 | awk ' { print $1 } ')
+
+#ffmpeg -strict -2 -codec h264 -i $1 -filter:v "crop=1706:960:107:60" Left-$$.mp4 
+#ffmpeg -strict -2 -codec h264 -i $2 -filter:v "crop=1706:960:107:60" Rigt-$$.mp4 
+
+#
+#ffmpeg -i Left-$$.mp4 -i Right-$$.mp4 -filter_complex \
+#"[0:v]setpts=PTS-STARTPTS, pad=iw*2:ih[bg]; \
+##[1:v]setpts=PTS-STARTPTS[fg]; [bg][fg]overlay=w; \
+#amerge,pan=stereo:c0<c0+c2:c1<c1+c3" output
+
