8cd9ac9776cb822d9243eb3d4793755b47754ea8
el-tocino <email+github@aforkinthetoad.com>
2015-11-09 18:07:17 -0600

    MOAR FIX3S!

--- syncotater.sh
+++ syncotater.sh
@@ -67,15 +67,24 @@ case ${RFR} in
 
 esac
 
+# count frames....
 LFC=$(ffprobe -i $1 -show_frames -hide_banner |grep coded_picture_number | tail -1 | cut -d= -f2 )
 RFC=$(ffprobe -i $2 -show_frames -hide_banner |grep coded_picture_number | tail -1 | cut -d= -f2 )
 
-LOF=$(python2 ${CLPR} -c -r ${LFR} $1 $2 | tail -1 | awk ' { print $1 } ' |cut -d: -f4)
-ROF=$(python2 ${CLPR} -c -r ${LFR} $2 $1 | tail -1 | awk ' { print $1 } ' |cut -d: -f4)
+# get frame offsets...
+FOFF=$(python2 ${CLPR} -c -r ${LFR} $1 $2 | tail -1 |awk ' { print $1 } ')
 
+# Using the second number's decimals would give a relative quality assessment. Closer to .5, the worse the offset.  
+
+# make this optional...
 # Trim video edges...on super wide angles should help the final rendering look better...
+# 1920x1080 -> 1706x960
+# 1280x720 -> 1138x640
+# 2k -> ?
+# other -> ???
 #ffmpeg -strict -2 -codec h264 -i $1 -filter:v "crop=1706:960:107:60" Left-$$.mp4 
 #ffmpeg -strict -2 -codec h264 -i $2 -filter:v "crop=1706:960:107:60" Rigt-$$.mp4 
+# on 1080p and wide angle video, offset cropping could make aligning a bit better as well. 
 #ffmpeg -strict -2 -codec h264 -i $1 -filter:v "crop=1706:960:110:60" Left-$$.mp4 
 #ffmpeg -strict -2 -codec h264 -i $2 -filter:v "crop=1706:960:104:60" Rigt-$$.mp4 
 
