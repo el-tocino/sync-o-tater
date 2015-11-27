a7dc8a8d173c73c45cff7f76ab2c2e06b06697a6
el-tocino <email+github@aforkinthetoad.com>
2015-11-27 12:44:25 -0600

    all the frames counted, integers and shit.

--- framecount.py
+++ framecount.py
@@ -1,10 +1,8 @@
-#!python
+#!/usr/bin/env python
 
 import sys
-import numpy as nm
 import cv2
 
-print ("First argument: %s" % str(sys.argv[1]))
 cap = cv2.VideoCapture(sys.argv[1])
 frc = cap.get(7)
-print ("%s\tTotal frames" % (frc) )
+print ("%4.0f\tTotal frames" % (frc) )
